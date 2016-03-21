fae56009cea65dfec5d0f235b8828cff0a19acba
Peter Bayiokos <peterbayio533@gmail.com>
2016-03-21 19:46:35 -0400

    Initial Commit

---
+++ SampleTwitterApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SampleTwitterApp
+//
+//  Created by Peter Bayiokos on 3/21/16.
+//  Copyright © 2016 Pace University. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
